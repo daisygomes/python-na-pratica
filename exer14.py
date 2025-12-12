8ee495c994ba02b0623b47fde534297785eaf2be
daisygomes <daisygomesca@icloud.com>
2025-12-12 00:38:33 -0300

    Add files via upload

---
+++ exer14.py
@@ -0,0 +1,5 @@
+# Salário de um funcionário com aumento de 15%
+
+salario = int(input("Digite o seu salário :"))
+novo_salario = salario + (salario * 0.15)
+print("O seu salário original era {} R$, mas com o aumento de 15% passa a ser {} R$." .format(salario, novo_salario))
